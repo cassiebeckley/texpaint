d86e3ca129c9b3158db8afb0cecd87378fa6b1b7
Cassandra Beckley <beckl.ds@gmail.com>
2020-03-31 09:34:36 -0700

    Load and draw images

--- shaders/imageShader/frag.glsl
+++ shaders/imageShader/frag.glsl
@@ -1,5 +1,9 @@
 precision mediump float;
 
+varying highp vec2 vTextureCoord;
+
+uniform sampler2D uSampler;
+
 void main() {
-    gl_FragColor = vec4(1, 0, 0.5, 1);
+    gl_FragColor = texture2D(uSampler, vTextureCoord);
 }
--- texpaint.js
+++ texpaint.js
@@ -11,51 +11,79 @@ let canvas = null;
 let gl = null;
 let shaders = {};
 let imagePositionBuffer = null;
+let imageTextureBuffer = null;
 const uiProjectionMatrix = mat4.create();
 const imageMatrix = mat4.create();
 
 const eventState = {
     mouseButtonsDown: [],
-    lastMousePosition: { x: 0, y: 0 }
+    lastMousePosition: vec3.create()
 };
 
-const currentImage = {
-    width: 640,
-    height: 480,
-    buffer: new Uint32Array(640 * 480)
+const createBlankImage = (width, height) => {
+    const buffer = new Uint8ClampedArray(width * height * 4);
+
+    buffer.fill(255);
+
+    return {
+        width,
+        height,
+        buffer,
+        texture: null
+    };
 };
 
-const imageVertexPositions = [
-    0, 0,
-    0, currentImage.height,
-    currentImage.width, 0,
-    currentImage.width, currentImage.height
+const currentImage = createBlankImage(640, 480);
+
+const imageTexturePositions = [
+    0.0, 0.0,
+    0.0, 1.0,
+    1.0, 0.0,
+    1.0, 1.0
 ];
 
 const draw = (ts) => {
     gl.clearColor(0.23, 0.23, 0.23, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // draw 2d image view
+    //// draw 2d image view ////
     gl.useProgram(shaders.imageShader.program);
 
     // set projection and model*view matrices;
     gl.uniformMatrix4fv(shaders.imageShader.uniforms.uProjectionMatrix, false, uiProjectionMatrix);
     gl.uniformMatrix4fv(shaders.imageShader.uniforms.uModelViewMatrix, false, imageMatrix);
 
-    gl.enableVertexAttribArray(imagePositionBuffer);
-    gl.bindBuffer(gl.ARRAY_BUFFER, imagePositionBuffer);
+    {
+        const size = 2;
+        const type = gl.FLOAT; // 32 bit floats
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, imagePositionBuffer);
+        gl.vertexAttribPointer(shaders.imageShader.attributes.aVertexPosition, size, type, normalize, stride, offset);
+        gl.enableVertexAttribArray(shaders.imageShader.attributes.aVertexPosition);
+    }
 
-    const size = 2;
-    const type = gl.FLOAT; // 32 bit floats
-    const normalize = false;
-    const stride = 0;
-    let offset = 0;
-    gl.vertexAttribPointer(shaders.imageShader.attributes.aVertexPosition, size, type, normalize, stride, offset);
+    {
+        const size = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, imageTextureBuffer);
+        gl.vertexAttribPointer(shaders.imageShader.attributes.aTextureCoord, size, type, normalize, stride, offset);
+        gl.enableVertexAttribArray(shaders.imageShader.attributes.aTextureCoord);
+    }
 
-    offset = 0;
-    const count = 4;
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, count);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, currentImage.texture);
+    gl.uniform1i(shaders.imageShader.uniforms.uSampler, 0);
+
+    {
+        const offset = 0;
+        const count = 4;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, count);
+    }
 
 };
 
@@ -65,12 +93,11 @@ const onAnimationFrame = (ts) => {
 };
 
 const calculateImagePosFromUI = (uiCoord) => {
-    const coord = vec3.create();
+    const imageCoord = vec3.create();
     const invImageMatrix = mat4.create()
     mat4.invert(invImageMatrix, imageMatrix);
-    vec3.set(coord, uiCoord.x, uiCoord.y, 0);
-    vec3.transformMat4(coord, coord, invImageMatrix);
-    return { x: coord[0], y: coord[1] };
+    vec3.transformMat4(imageCoord, uiCoord, invImageMatrix);
+    return imageCoord;
 };
 
 const handleResize = () => {
@@ -94,31 +121,74 @@ const handleWheel = (e) => {
 
         // Scale with mouse as origin
         const imageMousePos = calculateImagePosFromUI(eventState.lastMousePosition);
-        mat4.translate(imageMatrix, imageMatrix, [imageMousePos.x, imageMousePos.y, 0]);
+        mat4.translate(imageMatrix, imageMatrix, imageMousePos);
         mat4.scale(imageMatrix, imageMatrix, [scaleFactor, scaleFactor, 1]);
-        mat4.translate(imageMatrix, imageMatrix, [-imageMousePos.x, -imageMousePos.y, 0]);
+
+        vec3.negate(imageMousePos, imageMousePos);
+        mat4.translate(imageMatrix, imageMatrix, imageMousePos);
     }
 };
 
 const handleMouseDown = (e) => {
     eventState.mouseButtonsDown[e.button] = true;
+
+    if (e.button === 1) { // MMV
+        document.body.style.cursor = 'grab';
+    }
 };
 
 const handleMouseUp = (e) => {
     eventState.mouseButtonsDown[e.button] = false;
+
+    if (e.button === 1) { // MMV
+        document.body.style.cursor = 'auto';
+    }
+}
+
+const mouseEventToVec3 = (e) => {
+    const coord = vec3.create();
+    vec3.set(coord, e.clientX, e.clientY, 0);
+    return coord;
 }
 
 const handleMouseMove = (e) => {
-    const deltaX = e.clientX - eventState.lastMousePosition.x;
-    const deltaY = e.clientY - eventState.lastMousePosition.y;
+    const currentMousePosition = mouseEventToVec3(e);
+    const delta = vec3.create();
+    vec3.sub(delta, currentMousePosition, eventState.lastMousePosition);
 
     // if MMB is down
     if (eventState.mouseButtonsDown[1]) {
-        mat4.translate(imageMatrix, imageMatrix, [deltaX, deltaY, 0]);
+        let deltaMouse = calculateImagePosFromUI(currentMousePosition);
+        let lastImageMousePos = calculateImagePosFromUI(eventState.lastMousePosition);
+        mat4.sub(deltaMouse, deltaMouse, lastImageMousePos);
+        mat4.translate(imageMatrix, imageMatrix, deltaMouse);
     }
 
-    eventState.lastMousePosition.x = e.clientX;
-    eventState.lastMousePosition.y = e.clientY;
+    eventState.lastMousePosition = currentMousePosition;
+};
+
+const handleKeyup = (e) => {
+    if (e.isComposing || e.keyCode === 229) {
+        return;
+    }
+
+    if (e.keyCode === 79) {
+        const fileSelector = document.getElementById('file-selector');
+        fileSelector.click();
+        fileSelector.addEventListener('change', function () {
+            const file = this.files[0];
+
+            if (!file.type.startsWith('image')) {
+                throw new Error('file is not an image');
+            }
+
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                tempImageTexture = loadImage(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        });
+    }
 };
 
 const loadShader = (type, source) => {
@@ -172,6 +242,69 @@ const loadShaderProgram = (vsSource, fsSource) => {
     };
 };
 
+const isPowerOf2 = (value) => (value & (value - 1)) == 0;
+
+const randRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const generateImageVertices = () => [
+    0, 0,
+    0, currentImage.height,
+    currentImage.width, 0,
+    currentImage.width, currentImage.height
+];
+
+const swapBuffer = () => {
+    // upload texture
+    gl.bindTexture(gl.TEXTURE_2D, currentImage.texture);
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = currentImage.width;
+    const height = currentImage.height;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, srcFormat, srcType, currentImage.buffer);
+};
+
+const loadImage = (url) => {
+    // parse image file
+    // we have to use Canvas as an intermediary
+    const tempImg = document.createElement('img');
+
+    // TODO: probably return Promise
+
+    tempImg.addEventListener('load', () => {
+        const tempImageCanvas = document.createElement('canvas');
+        tempImageCanvas.width = tempImg.width;
+        tempImageCanvas.height = tempImg.height;
+        const ctx = tempImageCanvas.getContext('2d');
+        ctx.drawImage(tempImg, 0, 0);
+        const imageData = ctx.getImageData(0, 0, tempImg.width, tempImg.height);
+        currentImage.buffer = imageData.data;
+        currentImage.width = imageData.width;
+        currentImage.height = imageData.height;
+
+        swapBuffer(currentImage.texture);
+        resetImageSize();
+
+        // update vertices in case width/height changed
+        gl.bindBuffer(gl.ARRAY_BUFFER, imagePositionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generateImageVertices()), gl.STATIC_DRAW);
+    });
+    tempImg.src = url;
+};
+
+const resetImageSize = () => {
+    gl.bindBuffer(gl.ARRAY_BUFFER, imagePositionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generateImageVertices()), gl.STATIC_DRAW);
+
+    //// initialize 2d image ////
+    mat4.identity(imageMatrix);
+    mat4.translate(imageMatrix, imageMatrix, [canvas.width / 2 - currentImage.width / 2, canvas.height / 2 - currentImage.height / 2, 0]);
+}
+
 const startRunning = () => {
     canvas = document.getElementById('application');
     gl = canvas.getContext('webgl');
@@ -188,20 +321,35 @@ const startRunning = () => {
 
     //// initialize buffers ////
     imagePositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, imagePositionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(imageVertexPositions), gl.STATIC_DRAW);
+
+    imageTextureBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, imageTextureBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(imageTexturePositions), gl.STATIC_DRAW);
+
+    const texture = gl.createTexture();
+    currentImage.texture = texture;
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
     //// add event listeners ////
     window.addEventListener('resize', handleResize);
+
+    //// reset canvas and image dimensions ////
+    swapBuffer();
     handleResize();
+    resetImageSize();
 
     canvas.addEventListener('wheel', handleWheel);
     canvas.addEventListener('mousedown', handleMouseDown);
     canvas.addEventListener('mouseup', handleMouseUp);
     canvas.addEventListener('mousemove', handleMouseMove);
 
-    //// initialize 2d image ////
-    mat4.translate(imageMatrix, imageMatrix, [canvas.width / 2 - currentImage.width / 2, canvas.height / 2 - currentImage.height / 2, 0]);
+    window.addEventListener('keyup', handleKeyup);
 
     //// start draw loop ////
     onAnimationFrame();
@@ -210,7 +358,7 @@ const startRunning = () => {
 const startRunningAndHandleErrors = () => {
     try {
         startRunning();
-    } catch(e) {
+    } catch (e) {
         const pre = document.createElement('pre');
         pre.textContent = e.message;
         document.body.innerHTML = '';
